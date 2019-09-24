51b3349031a7d8025951e45c74f9fc336ff997ac
Vendrell <oriol.vendrell.gallart@hp.com>
2019-09-24 11:36:33 +0200

    add median function

--- stats.go
+++ stats.go
@@ -1,5 +1,9 @@
 package stats
 
+import (
+	"sort"
+)
+
 // Mean returns the mean of the slice.
 func Mean(input []float64) float64 {
 	if len(input) == 0 {
@@ -10,4 +14,25 @@ func Mean(input []float64) float64 {
 		sum += in
 	}
 	return sum / float64(len(input))
+}
+
+// Median returns the median of the slice
+func Median(input []float64) (output float64) {
+	if len(input) == 0 {
+		return 0
+	}
+	if len(input) == 1 {
+		return input[0]
+	}
+
+	sort.Float64s(input)
+	l := len(input)
+
+	if l%2 != 0 {
+		output = input[l/2]
+	} else {
+		output = (input[l/2] + input[l/2-1]) / 2
+	}
+
+	return output
 }
--- stats_test.go
+++ stats_test.go
@@ -44,3 +44,47 @@ func BenchmarkMean2(b *testing.B) { benchmarkMean(2, b) }
 func BenchmarkMean3(b *testing.B) { benchmarkMean(10, b) }
 func BenchmarkMean4(b *testing.B) { benchmarkMean(1e3, b) }
 func BenchmarkMean5(b *testing.B) { benchmarkMean(1e6, b) }
+
+func TestMedian(t *testing.T) {
+	type args struct {
+		input []float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"even case sorted", args{[]float64{1.0, 2.0, 3.0, 4.0}}, 2.5},
+		{"odd case sorted", args{[]float64{1.0, 2.0, 3.0, 4.0, 5.0}}, 3.0},
+		{"even case unsorted", args{[]float64{4.0, 2.0, 1.0, 3.0}}, 2.5},
+		{"odd case unsorted", args{[]float64{4.0, 3.0, 5.0, 1.0, 2.0}}, 3.0},
+		{"empty case", args{[]float64{}}, 0},
+		{"single value case", args{[]float64{1.0}}, 1.0},
+		{"two values case", args{[]float64{1.0, 9.0}}, 5.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Median(tt.args.input); got != tt.want {
+				t.Errorf("Median() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func benchmarkMedian(len int, b *testing.B) {
+	s := make([]float64, len)
+	for e := 0; e <= len-1; e++ {
+		s[e] = rand.Float64()
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Median(s)
+	}
+}
+
+func BenchmarkMedian0(b *testing.B)   { benchmarkMean(0, b) }
+func BenchmarkMedian1(b *testing.B)   { benchmarkMean(1, b) }
+func BenchmarkMedian2(b *testing.B)   { benchmarkMean(2, b) }
+func BenchmarkMedian10(b *testing.B)  { benchmarkMean(10, b) }
+func BenchmarkMedian1e3(b *testing.B) { benchmarkMean(1e3, b) }
+func BenchmarkMedian1e6(b *testing.B) { benchmarkMean(1e6, b) }
