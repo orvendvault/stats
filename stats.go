56f0e639a9671337ad8d84528b448cf7e8f6085c
Vendrell <oriol.vendrell.gallart@hp.com>
2019-09-25 09:48:07 +0200

    implement new functions

--- stats.go
+++ stats.go
@@ -1,5 +1,13 @@
 package stats
 
+import (
+	"math"
+)
+
+/*
+   In this file there are implemented basic statistical functionalities to perform descriptive statistical analysis on a sample
+*/
+
 // Mean returns the mean of the slice.
 func Mean(input []float64) float64 {
 	if len(input) == 0 {
@@ -10,4 +18,121 @@ func Mean(input []float64) float64 {
 		sum += in
 	}
 	return sum / float64(len(input))
+}
+
+// Max returns the maximum value of the sample
+func Max(input []float64) float64 {
+	if len(input) == 0 {
+		return 0
+	}
+	max := input[0]
+	for i := 1; i < len(input); i++ {
+		if input[i] > max {
+			max = input[i]
+		}
+	}
+	return max
+}
+
+// Min returns the minimum value of the sample
+func Min(input []float64) float64 {
+	if len(input) == 0 {
+		return 0
+	}
+	min := input[0]
+	for i := 1; i < len(input); i++ {
+		if input[i] < min {
+			min = input[i]
+		}
+	}
+	return min
+}
+
+// Range returns the difference between the largest and smallest values
+func Range(input []float64) float64 {
+	if len(input) == 0 {
+		return 0
+	}
+	if len(input) == 1 {
+		return 0
+	}
+	return Max(input) - Min(input)
+}
+
+// sumOfSquaredDifferences returns the sum of the squared differences of each observation from the mean
+func sumOfSquaredDifferences(input []float64) float64 {
+	if len(input) == 0 {
+		return 0
+	}
+	if len(input) == 1 {
+		return 0
+	}
+	mean := Mean(input)
+	ssd := 0.0
+	for _, o := range input {
+		ssd += math.Pow(o-mean, 2.0)
+	}
+	return ssd
+}
+
+// StandardDeviationSample returns the standard deviation of the sample
+func StandardDeviationSample(input []float64) float64 {
+	return math.Sqrt(VarianceSample(input))
+}
+
+// VarianceSample returns the variance of the sample
+func VarianceSample(input []float64) float64 {
+	if len(input) == 0 {
+		return 0
+	}
+	if len(input) == 1 {
+		return 0
+	}
+	num := sumOfSquaredDifferences(input)
+	den := float64(len(input) - 1)
+	return num / den
+}
+
+// Quartile1 returns the first quartile
+func Quartile1(input []float64) (Q1 float64) {
+	if len(input) == 0 {
+		return 0
+	}
+	if len(input) == 1 {
+		return 0
+	}
+
+	if len(input)%2 == 0 {
+		Q1 = Median(input[:len(input)/2-1])
+	} else {
+		Q1 = Median(input[:len(input)/2-1])
+	}
+	return Q1
+}
+
+// Quartile2 returns the second quartile (equivalent to the median)
+func Quartile2(input []float64) float64 {
+	return Median(input)
+}
+
+// Quartile3 returns the third quartile
+func Quartile3(input []float64) (Q3 float64) {
+	if len(input) == 0 {
+		return 0
+	}
+	if len(input) == 1 {
+		return 0
+	}
+
+	if len(input)%2 == 0 {
+		Q3 = Median(input[len(input)/2:])
+	} else {
+		Q3 = Median(input[len(input)/2+1:])
+	}
+	return Q3
+}
+
+// InterQuartileRange returns the difference between the third and the first quartiles
+func InterQuartileRange(input []float64) float64 {
+	return Quartile3(input) - Quartile1(input)
 }
--- stats_test.go
+++ stats_test.go
@@ -1,6 +1,7 @@
 package stats
 
 import (
+	"math"
 	"math/rand"
 	"testing"
 )
@@ -44,3 +45,113 @@ func BenchmarkMean2(b *testing.B) { benchmarkMean(2, b) }
 func BenchmarkMean3(b *testing.B) { benchmarkMean(10, b) }
 func BenchmarkMean4(b *testing.B) { benchmarkMean(1e3, b) }
 func BenchmarkMean5(b *testing.B) { benchmarkMean(1e6, b) }
+
+func TestMax(t *testing.T) {
+	type args struct {
+		input []float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"empty case", args{[]float64{}}, 0.0},
+		{"equal case", args{[]float64{5.0, 5.0, 5.0, 5.0}}, 5.0},
+		{"normal case", args{[]float64{1.0, 5.0, 2.0, 6.0, 1.0, 2.0, 3.0}}, 6.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Max(tt.args.input); got != tt.want {
+				t.Errorf("Max() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMin(t *testing.T) {
+	type args struct {
+		input []float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"empty case", args{[]float64{}}, 0.0},
+		{"equal case", args{[]float64{5.0, 5.0, 5.0, 5.0}}, 5.0},
+		{"normal case", args{[]float64{1.0, 5.0, 2.0, 6.0, 1.0, 2.0, 3.0}}, 1.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Min(tt.args.input); got != tt.want {
+				t.Errorf("Min() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRange(t *testing.T) {
+	type args struct {
+		input []float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"empty case", args{[]float64{}}, 0.0},
+		{"equal case", args{[]float64{5.0, 5.0, 5.0, 5.0}}, 0.0},
+		{"normal case", args{[]float64{1.0, 5.0, 2.0, 6.0, 1.0, 2.0, 3.0}}, 5.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Range(tt.args.input); got != tt.want {
+				t.Errorf("Range() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStandardDeviationSample(t *testing.T) {
+	type args struct {
+		input []float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"empty case", args{[]float64{}}, 0.0},
+		{"single value case", args{[]float64{1.0}}, 0.0},
+		{"normal case", args{[]float64{1.0, 2.0, 3.0, 2.3, 1.4, 1.7, 1.5, 1.5, 1.8, 2.6, 2.3, 2.0, 2.2}}, 0.542548},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := StandardDeviationSample(tt.args.input); math.Abs(got-tt.want) > 1e6 {
+				t.Errorf("StandardDeviationSample() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestVarianceSample(t *testing.T) {
+	type args struct {
+		input []float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"empty case", args{[]float64{}}, 0.0},
+		{"single value case", args{[]float64{1.0}}, 0.0},
+		{"normal case", args{[]float64{1.0, 2.0, 3.0, 2.3, 1.4, 1.7, 1.5, 1.5, 1.8, 2.6, 2.3, 2.0, 2.2}}, 0.294358},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := VarianceSample(tt.args.input); math.Abs(got-tt.want) > 1e6 {
+				t.Errorf("VarianceSample() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
