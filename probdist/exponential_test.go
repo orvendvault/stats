cd2b1fb5394fea3898c78e73f688cb4ae64fb6d7
Vendrell <oriol.vendrell.gallart@hp.com>
2019-10-07 12:00:38 +0200

    add exp cdf test and benchmark

--- probdist/exponential_test.go
+++ probdist/exponential_test.go
@@ -6,6 +6,52 @@ import (
 	"testing"
 )
 
+func TestExponential_CDF(t *testing.T) {
+	type args struct {
+		x float64
+	}
+	tests := []struct {
+		name string
+		exp  Exponential
+		args args
+		want float64
+	}{
+		{"NaN case lambda", Exponential{math.NaN()}, args{0.2}, math.NaN()},
+		{"NaN case args", Exponential{0.5}, args{math.NaN()}, math.NaN()},
+		{"Normal case", Exponential{0.5}, args{2.0}, 0.632120},
+		{"Normal case 2", Exponential{0.5}, args{0.2}, 0.095162},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.exp.CDF(tt.args.x)
+			if math.IsNaN(got) || math.IsNaN(tt.want) {
+				if !math.IsNaN(got) || !math.IsNaN(tt.want) {
+					t.Errorf("CDF() = %v, want %v", got, tt.want)
+				}
+			} else if math.Abs(got-tt.want) > 1e-6 {
+				t.Errorf("CDF() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func benchmarkExponentialCDF(seed1 int64, b *testing.B) {
+	exp := Exponential{}
+	rand.Seed(seed1)
+	exp.Lambda = rand.Float64() * 2
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		exp.CDF(rand.Float64() * 10)
+	}
+}
+
+func Benchmark_exponential_CDF1(b *testing.B) { benchmarkExponentialCDF(0, b) }
+func Benchmark_exponential_CDF2(b *testing.B) { benchmarkExponentialCDF(25, b) }
+func Benchmark_exponential_CDF3(b *testing.B) { benchmarkExponentialCDF(15, b) }
+func Benchmark_exponential_CDF4(b *testing.B) { benchmarkExponentialCDF(3, b) }
+func Benchmark_exponential_CDF5(b *testing.B) { benchmarkExponentialCDF(1e3, b) }
+func Benchmark_exponential_CDF6(b *testing.B) { benchmarkExponentialCDF(534, b) }
+
 func Test_exponential_Mean(t *testing.T) {
 	tests := []struct {
 		name string
